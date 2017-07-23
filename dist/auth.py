16c9d3dcfe9c6500b8cdb35f7f407c8123908d7c
Andresito de Guzman <andresitomyemail@gmail.com>
2017-07-23 08:40:32 +0800

    Added twtpy2

--- dist/auth.py
+++ dist/auth.py
@@ -1,6 +1,6 @@
 # Put all credentials here
 
-consumer_key = "1OjFia61tz6Sa61ooVnQ1r3vm"
-consumer_secret = "pQZFjJ3dSHIP5qtySN3kdl6a9OS9Ehr4jgJOqrrnGJvy6UQqIO"
-access_token_key = "817642977413644288-k79SqFqlopSveNhz9pzvVjVDUD39nFC"
-access_token_secret = "N1P1cJIjHic6FDzD4IxKo07Y3jvji130KaSLp3IaKXQ72"
+consumer_key = ""
+consumer_secret = ""
+access_token_key = ""
+access_token_secret = ""
---
+++ twtpy2/twtpy.py
@@ -0,0 +1,10 @@
+# TwtPy 2
+# Andresito de Guzman
+# 2017
+import json
+
+def init():
+    return 'ok'
+
+if __name__ == "__main__":
+    init()
