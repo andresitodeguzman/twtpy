0118316df964c09198747255f9f3339ed736066d
Andresito de Guzman <deguzman.andresito@gmail.com>
2018-02-08 14:11:45 +0800

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,14 @@
+# TweetPy
+# Test
+
+import unittest
+import tweet
+
+class SampleTestClass(unittest.TestCase):
+
+  def sampleTest(self):
+    #do something
+    a = 1
+
+if __name__ == '__main__':
+  unittest.main()
